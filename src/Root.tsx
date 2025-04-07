9481346a78fd459fa8a6e93cc632440386c635ea
Daria Radtkina <dasharadtkina@gmail.com>
2025-04-07 17:31:54 +0300

    Solution

--- src/Root.tsx
+++ src/Root.tsx
@@ -13,10 +13,12 @@ export const Root = () => (
   <Router>
     <Routes>
       <Route path="/" element={<App />}>
-        <Route index element={<HomePage />}></Route>
+        <Route index element={<HomePage />} />
         <Route path="/home" element={<Navigate to="/" replace />} />
-        <Route path="/people" element={<PeoplePage />} />
-        <Route path="/people/:slug" element={<PeoplePage />} />
+        <Route path="/people">
+          <Route index element={<PeoplePage />} />
+          <Route path=":slug" element={<PeoplePage />} />
+        </Route>
         <Route path="*" element={<NotFoundPage />}></Route>
       </Route>
     </Routes>
