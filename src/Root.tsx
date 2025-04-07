48b7ff01489c9441f50469e62f030f6359e12379
Daria Radtkina <dasharadtkina@gmail.com>
2025-04-07 17:44:11 +0300

    Solution

--- src/Root.tsx
+++ src/Root.tsx
@@ -16,8 +16,7 @@ export const Root = () => (
         <Route index element={<HomePage />} />
         <Route path="/home" element={<Navigate to="/" replace />} />
         <Route path="/people">
-          <Route index element={<PeoplePage />} />
-          <Route path=":slug" element={<PeoplePage />} />
+          <Route path=":slug?" element={<PeoplePage />} />
         </Route>
         <Route path="*" element={<NotFoundPage />}></Route>
       </Route>
